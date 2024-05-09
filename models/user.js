df3c8d1790f8295b35d1c087519f3d6024d7f792
wellheythereMJ <abetterplace@outlook.com>
2024-05-08 18:31:42 -0700

    updated server, connect, and user js files

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const {Schema, model} = require("mongoose")
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+  }, 
+  thoughts: {
+    type: Schema.Types.ObjectId,
+    ref: 'Thought',
+  },
+  friends: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+}
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
+
